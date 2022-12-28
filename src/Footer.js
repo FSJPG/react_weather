a77416ca115c9390c17224cf14643daef2d1586b
Forough <121292583+FSJPG@users.noreply.github.com>
2022-12-28 11:26:38 +0100

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
       <div className="col-lg-8 offset-lg-2 text-center p-0">
         <hr />
         <p className="opacity-75">
-          <a href="https://github.com/FSJPG/react_weather_api">Open-source</a>{" "}
+          <a href="https://github.com/FSJPG/react_weather">Open-source</a>{" "}
           code by Forough
         </p>
       </div>
